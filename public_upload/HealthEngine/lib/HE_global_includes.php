7aa6513c26c21aa5ddd274795d87b5da6bb0599f
mshoward <mshoward42@gmail.com>
2015-03-25 18:17:41 -0500

    test 3

--- public_upload/HealthEngine/lib/HE_global_includes.php
+++ public_upload/HealthEngine/lib/HE_global_includes.php
@@ -3,7 +3,8 @@
 	require_once "/var/data/public/HealthEngine/lib/debugging/logger.php";
 	$GLOBALS["HE_LOGGER"] = new logger();
 	$llogger = & $GLOBALS["HE_LOGGER"];
-
+	echo "this far<br>";
+	$lloger->shout(3,"testing the shout");
 	$llogger->logme(3, "Including webroot/library/globals.inc.php");
 	require_once "/var/data/public/library/globals.inc.php";
 	$llogger->logme(3, "Including webroot/interface/globals.php");
